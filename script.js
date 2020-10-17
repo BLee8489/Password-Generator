35e70f2a3e65dd57e06b072c88cf9642cb151bed
“bonnielee” <bonlee8489@gmail.com>
2020-10-17 14:59:56 -0700

    final edit to JS

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ generateBtn.addEventListener("click", writePassword);
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var specialcharacters = ["!", "*", ".", "$", "/", "?", "@", "&"]
-var numerics = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+var numerics = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 function generatePassword() {
     var possibles = []
 
     var length = parseInt(prompt("How long would you like your password to be?"))
-    // length = parseInt(length);
+
     if (length < 8 || length > 128) {
         alert("Please make the length more than 8 characters and less than 128 characters")
+        return ""
     } else {
 
 
@@ -36,22 +36,22 @@ function generatePassword() {
         var confirmSymbols = confirm("Would you like any special characters?");
         var confirmNumbers = confirm("Would you like any numbers?");
 
-        if (confirmLower) possibles.push(lowercase)
-        if (confirmUpper) possibles.push(uppercase)
-        if (confirmSymbols) possibles.push(specialcharacters)
-        if (confirmNumbers) possibles.push(numerics)
+        if (confirmLower) possibles.push.apply(possibles, lowercase);
+        if (confirmUpper) possibles.push.apply(possibles, uppercase);
+        if (confirmSymbols) possibles.push.apply(possibles, specialcharacters);
+        if (confirmNumbers) possibles.push.apply(possibles, numerics);
 
 
-        var password = ""
+        var password = "";
 
-        while (password.length < length) {
-            for (let i = 0; i < possibles.length; i++) {
-                if (password.length < length) {
-                    let rand = Math.floor(Math.random() * possibles[i].length)
-                    password += possibles[i][rand]
-                }
-            }
-            return possibles
+        // while (password.length < length) {
+        for (let i = 0; i < length; i++) {
+            // if (password.length < length) {
+            let rand = Math.floor(Math.random() * possibles.length);
+            password = password + possibles[rand];
+
         }
+        return password
+        // }
     }
 }
